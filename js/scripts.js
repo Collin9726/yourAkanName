deed92db5f47b4dfae058b1145f2252fc18a42e1
collin9726 <owino.collin@gmail.com>
2020-03-25 17:02:18 +0300

    add form reset and feedback on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
+var bday;
+var radio1;
+var radio2;
+var dd;
+var mm;
+var yyyy;
+
 function validate(){
-    var bday=document.getElementById("birthday").value;
-    var radio1=document.getElementById("radio1");
-    var radio2=document.getElementById("radio2");
-    var dd=parseInt(bday.trim().slice(0,2));
-    var mm=parseInt(bday.trim().slice(3,5));
-    var yyyy=parseInt(bday.trim().slice(6,10));
+    bday=document.getElementById("birthday").value;
+    radio1=document.getElementById("radio1");
+    radio2=document.getElementById("radio2");
+    dd=parseInt(bday.trim().slice(0,2));
+    mm=parseInt(bday.trim().slice(3,5));
+    yyyy=parseInt(bday.trim().slice(6,10));
 
     if (bday.trim()===""){
         alert("Blank birthday input");
@@ -41,16 +48,28 @@ function validate(){
     }
 
     if (radio1.checked===true){
-        alert("Male chosen");
-        alert(yyyy);
+        //alert("Male chosen");
+        //alert(bday);        
+        document.getElementById("myform").reset();
+        dayWeek();
         return true;
     }
     else if (radio2.checked===true){
         alert("Female chosen");
+        document.getElementById("myform").reset();
         return true;
     }
     else{
         alert("Gender input missing");
-        return false;;
-    }
+        return false;
+    }    
+}
+
+function dayWeek(){ 
+    var CC=parseInt(yyyy.toString().slice(0,2));
+    var YY=parseInt(yyyy.toString().slice(2,4));
+    var MM=mm;
+    var DD=dd;
+    var dayOfWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7; 
+    document.getElementById("akanBirthday").innerHTML="Your Akan Birthday: "+bday.trim();
 }
